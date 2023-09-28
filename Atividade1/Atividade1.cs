38170a47b43ed0dfecaffe5b9bd013fa0b1f4bac
ninjabsbx <ninjabsbx@gmail.com>
2023-09-27 22:37:50 -0300

    Add project files.

---
+++ Atividade1/Atividade1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+int x, y;
+
+
+do
+{
+    string[] valores = Console.ReadLine().Split(' ');
+    x = int.Parse(valores[0]);
+    y = int.Parse(valores[1]);
+
+    if (x > 0 && y > 0)
+    {
+        Console.WriteLine("Primeiro Quadrante");
+    }
+    else if (x < 0 && y > 0)
+    {
+        Console.WriteLine("Segundo Quadrante");
+    }
+    else if (x < 0 && y < 0) 
+    {
+        Console.WriteLine("Terceiro Quadrante");
+    }
+    else if (x > 0 && y < 0)
+    {
+        Console.WriteLine("Quarto Quadrante");
+    }
+}
+while (x != 0 && y != 0);
+
---
+++ Atividade2/Atividade2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int N = int.Parse(Console.ReadLine());
+
+        int In = 0;
+        int Out = 0;
+
+        for (int i = 0; i < N; i++)
+        {
+            int X = int.Parse(Console.ReadLine());
+
+            if (X >= 10 && X <= 20)
+            {
+                In++;
+            }
+            else
+            {
+                Out++;
+            }
+        }
+
+        Console.WriteLine("Valores dentro do intervalo: " + In);
+        Console.WriteLine("Valores fora do intervalo:  " + Out);
+    }
+}
